c8649b5c54c2f0df3d69274f2c56ce813fad9e51
unknown <priyarvs@gmail.com>
2026-08-15 00:03:26 +0530

    -m

---
+++ crewtech.java
@@ -0,0 +1,14 @@
+public class crewtech {
+	public void myflight() {
+		System.out.println("myflight team size 4");
+	}
+
+	public void linkapps() {
+		System.out.println("linkapps team size 20");
+	}
+
+	public void manuals() {
+		System.out.println("manuals team size 4");
+	}
+
+}
